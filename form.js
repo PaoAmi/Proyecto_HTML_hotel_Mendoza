f2ca7e318e8a4c27725efd509e7d699d7af2e0cf
PaoAmi <144964507+PaoAmi@users.noreply.github.com>
2023-10-20 06:45:21 -0300

    Add files via upload

--- form.js
+++ form.js
@@ -12,21 +12,32 @@ function validar() {
     }
 
 
-    if (document.formulario.correo.value == "" || !(/^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/.test(document.formulario.correo.value)) ) {
+    if (document.formulario.correo.value == "" || !(/^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/.test(document.formulario.correo.value))) {
         alert("Completa un correo electrónico válido");
         document.formulario.correo.focus();
         return;
     }
-    
 
-    if (document.formulario.telefono.value == "" || !(/^\d{10}$/.test(document.formulario.telefono.value) )) {
+
+    if (document.formulario.telefono.value == "" || !(/^\d{7,10}$/.test(document.formulario.telefono.value))) {
         alert("Completa tu teléfono");
         document.formulario.telefono.focus();
         return;
     }
 
+    if (document.formulario.dateIngreso.value < "2000-01-01" || document.formulario.dateEgreso.value < "2000-01-01") {
+        alert("Completa una fecha");
+        document.formulario.dateIngreso.focus();
+        return;
+        
+    } else if (document.formulario.dateIngreso.value > document.formulario.dateEgreso.value) {
+        alert("La fecha de ingreso no puede ser posterior a la de egreso.");
+        document.formulario.dateEgreso.focus();
+        return;
+    }
 
-    if (document.formulario.select.selectedIndex == 0 ) {
+
+    if (document.formulario.select.selectedIndex == 0) {
         alert("Completa una opción");
         document.formulario.select.focus();
         return;
@@ -36,13 +47,13 @@ function validar() {
         alert("Completá el campo comentarios");
         document.formulario.select.focus();
         return;
-        
+
     }
 
     alert("Su formulario fue enviado");
     document.formulario.submit();
 
 }
-    
+
 
 
